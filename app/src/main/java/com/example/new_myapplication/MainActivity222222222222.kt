faec44c3ec5b8ad1245d42f64dfcbbe80b34b383
NahlaFadl <nahla.fadl2002@gmail.com>
2022-03-11 01:21:35 +0200

    22222222

--- app/src/main/java/com/example/new_myapplication/MainActivity222222222222.kt
+++ app/src/main/java/com/example/new_myapplication/MainActivity222222222222.kt
@@ -2,10 +2,12 @@ package com.example.new_myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity222222222222 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main222222222222)
+        Toast.makeText(this,"2222222222222",Toast.LENGTH_SHORT).show()
     }
 }
