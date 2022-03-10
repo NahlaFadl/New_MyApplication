b57cf8c208a4791e202679fd615a7f6060dba532
NahlaFadl <nahla.fadl2002@gmail.com>
2022-03-11 00:50:48 +0200

    newActivity

---
+++ app/src/main/java/com/example/new_myapplication/MainActivity2285.kt
@@ -0,0 +1,11 @@
+package com.example.new_myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2285 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2285)
+    }
+}
